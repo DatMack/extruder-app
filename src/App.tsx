5eb6f65531d8af615ed669ace0639716578b6b81
John Schloss <j.schloss09@icloud.com>
2022-12-20 21:21:14 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header>
+          <img className='logo' src="PacMoore_logo.png" alt="https://placekitten.com/200/300" />
+          <img className='profile' src="https://placekitten.com/200/300" alt="" />
+        </header>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/header.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function taskbar() {
+  return(
+    <div className='taskbar'>
+      <p>Hello Worlds</p>
+      <header><img src="PacMoore_logo.png" alt="" />PacMoore</header>
+    </div>
+  )
+}
+
+export default taskbar
